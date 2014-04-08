5ca3bb396a76e7c7d8e7b2298bc956842765119f
James Marquez <jamesmarquez@gmail.com>
2014-04-08 20:52:38 +0800

    updating main front page file

--- wp-content/themes/tcs-responsive/front.php
+++ wp-content/themes/tcs-responsive/front.php
@@ -33,70 +33,51 @@ else:
 <?php 
 global $activity;
 //$activity = get_activity_summary();
-
 ?>
 <div class="content">
-	<div id="main">
-		<div id="banner">
-			<div class="inner">
-				<div class="container">
-					<ul class="slider">
-						<?php 
-						$args = array (
+	<div id="banner">
+		<div class="container">
+			<ul class="slider">
+				<?php 
+					$args = array (
+						'post_type' 	=> 'promo',
+						'category_name' => 'Promo Banners'
+					);
 
-								'post_type' => 'promo',
-								'category_name' => 'Promo home'
-
-						);
-
-						$promos = new WP_Query ( $args );
-							
-						if ($promos->have_posts ()) :
-
+					$promos = new WP_Query ( $args );							
+					if ($promos->have_posts ()) :
 						while ( $promos->have_posts () ) :
-
-						$promos->the_post ();
-						?>
-						<li class="<?php echo strtolower ($post->post_title);?>"><?php the_content(); ?>
-						</li>
-						<?php endwhile; endif; wp_reset_query();?>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div id="stats">
-			<div class="container">
-				<p>
-					<em class="members"><?php echo get_activity_summary("member_count"); ?></em> of the world's best minds competing
-
-				</p>
-				<a class="btn btnAlt" href="<?php bloginfo('wpurl') ?>/challenges">View Challenges</a>
-			</div>
-		</div>
-		<!-- /#stats -->
-
-
-
-
-		<article id="mainContent">
-			<div class="container">
-				<?php					
-				if (have_posts ()) :
-				while ( have_posts () ) : the_post ();
-				$pid= $post->ID;
+							$promos->the_post ();
+							
+							// featured image attributes
+							$attr = array(
+								'alt'	=> trim(strip_tags( $post->post_title )),
+							);
+							
 				?>
-				<?php the_content();?>
-			</div>
-		</article>
-		<!-- /#mainContent -->
-		<article id="featuredContent">
-			<div class="container">
-				<?php echo do_shortcode(get_post_meta($pid,'Featured content',true)); ?>
-			</div>
-		</article>
-		<!-- /#featuredContent -->
+				<li class="<?php echo strtolower ($post->post_title);?>">
+					<a href="<?php the_permalink(); ?>"> <?php the_post_thumbnail('full', $attr); ?> </a>
+				</li>
+				<?php endwhile; endif; wp_reset_query();?>
+			</ul>
+		</div>
+	</div>		
+	<!-- /#promo-banner -->
 
-		<?php endwhile;  endif; wp_reset_query(); ?>
+	<div class="article">
+		
+		<?php					
+			if (have_posts ()) :
+				while ( have_posts () ) : 
+					the_post ();
+					the_content();
+				endwhile;
+			endif;
+			wp_reset_query();
+		?>
+		
+	</div>
+	<!-- /.article -->
 
 <?php get_footer(); ?>
 
