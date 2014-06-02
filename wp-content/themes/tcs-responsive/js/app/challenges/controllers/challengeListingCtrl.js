d44201e3178a501b7bbf0ac36741816b1be3f4dc
James Marquez <jamesmarquez@gmail.com>
2014-06-03 03:23:24 +0800

    Challenge Listing Angular Refactor Technology Tags

--- wp-content/themes/tcs-responsive/js/app/challenges/controllers/challengeListingCtrl.js
+++ wp-content/themes/tcs-responsive/js/app/challenges/controllers/challengeListingCtrl.js
@@ -1,4 +1,41 @@
-'use strict';
+/*global angular: true */
+(function (angular) {
+  'use strict';
+  var challengesModule = angular.module('tc.challenges');
+  challengesModule.controller('ChallengeListingCtrl', ['$scope', 'ChallengesService', 'DataService', '$http', '$window', 'TemplateService', 'GridService', '$routeParams',
+    function ($scope, ChallengesService, DataService, $http, $window, TemplateService, GridService, $routeParam) {
+      //console.log('routes', $routeParam);
+      $scope.allChallenges = [];
+      $scope.challenges = [];
+      $scope.filteredChallenges = [];
+      $scope.contest = {
+        contestType: '',
+        listType: 'active'
+      };
+      $scope.titles = {
+        '': 'All Open Challenges',
+        design: 'Graphic Design Challenges',
+        develop: 'Software Development Challenges',
+        data: 'Data Science Challenges'
+      };
+      $scope.view = 'table';
+      //$scope.wordpressConfig = $window.wordpressConfig;
+      $scope.getTrackSymbol = TemplateService.getTrackSymbol;
+      $scope.formatTimeLeft = TemplateService.formatTimeLeft;
+      $scope.getContestDuration = TemplateService.getContestDuration;
+      $scope.getPhaseName = TemplateService.getPhaseName;
+      $scope.dateFormat = 'dd MMM yyyy hh:mm EDT';
+      $scope.images = $window.wordpressConfig.stylesheetDirectoryUri + '/i/';
+      $scope.definitions = GridService.definitions($scope.contest);
+      $scope.gridOptions = GridService.gridOptions('definitions');
+      $scope.search = {
+        radioFilterChallenge: 'all',
+        show: false,
+        allPlatforms: [],
+        allTechnologies: []
+      };
+      $scope.pageSize = 10;
+      $scope.page = 1;
 
 tc.controller('ChallengeListingCtrl', ['$scope', 'Challenge',
   function($scope, Challenge) {
@@ -41,8 +78,59 @@ tc.controller('ChallengeListingCtrl', ['$scope', 'Challenge',
     ]
   };
 
-  // @TODO make type dynamic by using $routeparams
-  Challenge.all('active').getList({type: 'design'}).then(function(challenges) {
-    $scope.challenges = challenges;
-  });
-}]);
+      $scope.submit = function () {
+        $scope.filteredChallenges = $scope.allChallenges.filter(function (contest) {
+          if ($scope.search.radioFilterChallenge !== 'all' && $scope.search.radioFilterChallenge !== contest.challengeType) {
+            return false;
+          }
+          if ($scope.search.fSDate && contest.submissionEndDate < $scope.search.fSDate) {
+            return false;
+          }
+          if ($scope.search.fEDate && contest.submissionEndDate > $scope.search.fEDate) {
+            return false;
+          }
+          if($scope.search.technologies && $scope.search.technologies.length > 0) {
+            for(var tech in $scope.search.technologies) {
+              if(contest.technologies && contest.technologies.length > 0 && contest.technologies[0] != '' && contest.technologies.indexOf(tech) == -1) {
+                return false;
+              }
+            }
+          } 
+          return true;
+        });
+
+        $scope.challenges = $scope.setPagingData($scope.filteredChallenges, $scope.page, $scope.pageSize);
+      };
+
+      DataService.one('technologies').get().then(function(data) {
+        if(data) {
+          $scope.search.allTechnologies = data.technologies;
+        }
+      });
+      
+      DataService.one('platforms').get().then(function(data) {
+        if(data) {
+          $scope.search.allPlatforms = data.platforms;
+        }
+      });
+
+
+
+      $scope.$watch('page', function () {
+        if($scope.page < 0) {
+          $scope.page = 0;
+        }
+        $scope.challenges = $scope.setPagingData($scope.filteredChallenges, $scope.page, $scope.pageSize);
+      });
+
+      $scope.$watchCollection('contest', function (contest) {
+        $scope.definitions = GridService.definitions(contest);
+        if (contest.listType === 'past') {
+          $scope.view = 'table';
+        }
+        getChallenges(contest);
+      });
+      getChallenges($scope.contest);
+
+    }]);
+}(angular));
