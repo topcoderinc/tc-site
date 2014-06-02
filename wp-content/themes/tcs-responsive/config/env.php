c6e63b8cbd9091c64b8229076892ef0c4d7a8344
James Marquez <jamesmarquez@gmail.com>
2014-06-03 04:18:15 +0800

    enabling env.php to tcdev10

---
+++ wp-content/themes/tcs-responsive/config/env.php
@@ -0,0 +1,9 @@
+<?php
+define("WP_SITEURL", "http://tcdev10.topcoder.com");
+define("WP_HOME", "http://tcdev10.topcoder.com");
+
+define('TC_API_URL', 'https://api.topcoder.com/v2');
+
+force_ssl_admin(false);
+force_ssl_login(false);
+?>
