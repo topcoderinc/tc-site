d3b7e1ee4146c0d2148f1c2a88cdc6b9dd60bae1
Neil Hastings <neil.hastings@gmail.com>
2014-07-07 11:44:20 -0400

    Remove old code.  Off to gitlab

--- wp-content/plugins/wp-all-import/actions/wp_loaded.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function pmxi_wp_loaded() {				
-	
-	@ini_set("max_input_time", PMXI_Plugin::getInstance()->getOption('max_input_time'));
-	@ini_set("max_execution_time", PMXI_Plugin::getInstance()->getOption('max_execution_time'));		
-		
-}
--- wp-content/themes/tcs-responsive/sidebar-blog.php
+++
@@ -1,1 +0,0 @@
-<?php dynamic_sidebar('Blog Sidebar'); ?>
